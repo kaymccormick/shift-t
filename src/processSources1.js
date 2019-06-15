21fc3d1384023f9ebc46b182953b42dad581e4d3
Kay McCormick <kay@kaymccormick.com>
2019-06-15 00:43:15 +0000

    updates

--- src/processSources1.js
+++ src/processSources1.js
@@ -1,6 +1,5 @@
 const fs = require('fs');
 const path = require('path');
-const Sequelize = require('seqluelize');
 
 const sources = JSON.parse(fs.readFileSync(path.join(__dirname, '../sources_1.json'), { encoding: 'utf-8' }));
 const out = {}
@@ -37,12 +36,38 @@ const [done, p1, p2] = fn(root, []);
 const commonDir = p2.join('/');
 
 const outx = { module: {} };
+const sources2 = { module: {} };
 Object.keys(sources.file).forEach(file => {
     const m1 = './' + path.relative(commonDir, file);
-    outx.module[m1] = { class: {} }
-    const f = outx.module[m1];
+    const x = {module: m1, imported: {}};
+    sources2.module[m1] = x;
+    Object.keys(sources.file[file].imported).forEach(name => {
+        let element = sources.file[file].imported[name];
+        const m2 = './' + path.relative(commonDir, element[0]);
+        x.imported[name] = [m2, element[1]];
+    });
+    x.class = sources.file[file].classes;
+});
+fs.writeFileSync('sources_2.json', JSON.stringify(sources2, null, 4), 'utf-8');
+const classes = {};
+Object.keys(sources2.module).forEach(module => {
+    let moduleElement = sources2.module[module];
+    let c1 = moduleElement.class;
+    Object.keys(c1).forEach(class_ => {
+        const v = c1[class_];
+        if(v.superSpec) {
+            const imp = moduleElement.imported[v.superSpec];
+            if(!imp) {
+                console.log(v.superSpec);
+            } else {
+                const [ module, default_ ] = imp;
+                console.log(module);
+            }
+        }
+    })
+});
+/*
     const outFile = {}
-
     const v = sources.file[file];
     Object.keys(v.classes).forEach(name => {
         const c = v.classes[name];
@@ -60,6 +85,7 @@ Object.keys(sources.file).forEach(file => {
                 } else {
                     name2 = y.exported[c.superSpec];
                 }
+
                 if(name2 === undefined) {
                     throw new Error(`${file} ${name} ${module} ${c.superSpec} ${default_} ${y.defaultExport}`);
                 }
@@ -70,7 +96,7 @@ Object.keys(sources.file).forEach(file => {
         }
     });
 });
-
+*/
 // Object.keys(outx.module).forEach(k => {
 //     Object.keys(outx.module[k].class)
 //         .filter(c => outx.module[k].class[c]
@@ -81,6 +107,6 @@ Object.keys(sources.file).forEach(file => {
 // });
 
 //console.log(outx.module['/local/home/jade/JsDev/docutils-t/src/parsers/rst/states/RSTState'].class.RSTState)
-process.stdout.write(JSON.stringify(outx, null, 4));
+//process.stdout.write(JSON.stringify(outx, null, 4));
 
 
