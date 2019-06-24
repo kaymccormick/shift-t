893007b9f58346d27547f37622c6cf1ce751c99b
Kay McCormick <kay@kaymccormick.com>
2019-06-24 02:43:36 +0000

    update

--- transforms/orm.ts
+++ transforms/orm.ts
@@ -9,44 +9,57 @@ import { fromNodes } from 'jscodeshift/src/Collection';
 // @ts-ignore
 module.exports = function (fileInfo, api, options) {
     const report = api.report;
-    const ast = parse(fileInfo.source);
+//    const ast = parse(fileInfo.source);
     const j = api.jscodeshift;
     
-    const c = j(ast);
-    console.log(c.find(j.ImportDeclaration, (n: namedTypes.ImportDeclaration) => {
-                return n.source.type === 'StringLiteral'
- 		}).size());
+    const c = j(fileInfo.source);
     const m = c.find(j.ImportDeclaration, (n: namedTypes.ImportDeclaration) => {
-                return n.source.type === 'Literal'
+    console.log(n);
+        return n.source.type === 'StringLiteral'
 		&& n.source && n.source.value === 'typeorm'
  		&& n.specifiers });
+		let map2: Map<string, string> = Map<string, string>();
     console.log(m.size());
-    const map1: Map<string, string> = Map<string, string>(List.of<SpecifierKind>(...m
-            .nodes()
-	.flatMap((n: namedTypes.ImportDeclaration): SpecifierKind[] =>
-	n.specifiers ))
+    let map1: Map<string, string> = Map<string, string>(
+    	  List.of<SpecifierKind>(...m.nodes()
+            .flatMap((n: namedTypes.ImportDeclaration): SpecifierKind[] =>
+                n.specifiers ))
             .map((specifier: SpecifierKind): string[] =>
                 specifier.type === 'ImportSpecifier' &&
                 specifier.imported && specifier.local ? [specifier.imported.name, specifier.local.name] : [])
-        .filter(x => x.length));
-	console.log(map1.toJS());
-    let entityName = '';
-    if(map1.has('Entity')) {
-        entityName = map1.get('Entity');
-    }
-    const fileNode = c.nodes()[0];
-    if(!map1.has('Entity')) {
-      console.log('here');
-        ast.program.body.splice(0, 0, j.importDeclaration([j.importSpecifier(j.identifier('Entity'))], j.literal('typeorm')));
-	}
+            .filter(x => x.length));
+    console.log('map1: ', map1.toJS());
     
-    c.find(j.ImportDeclaration, (n) => n.importKind === 'value' && n.source.type === 'StringLiteral' && n.source.value === 'typeorm').forEach((p: NodePath) => {
-        //console.log(p.value.type);
+    const imports = ['Column', 'Entity'];
+    imports.forEach(importName => {
+    if(!map1.has(importName) || map1.get(importName) === undefined) {
+       map2 = map2.set(importName, importName);
+    }
     });
-    //    c.find(j.ClassDeclaration).forEach((p: NodePath) => {
-    //      const classDecl = p.value;
-    return print(ast).code;
-//    return c.toSource();
-//    return api.jscodeshift(
-//    return print(c.nodes()[0]).code;
+    if(map2.count()) {
+    console.log(map2.toJS());
+    c.nodes()[0].program.body.splice(0, 0, j.importDeclaration(map2.map((importedName, importName) => j.importSpecifier(j.identifier(importedName), j.identifier(importName))).valueSeq().toJS(), j.literal('typeorm')));
+    }
+    map1 = map1.merge(map2);
+    console.log(map1.toJS())
+
+/*    c.find(j.ClassDeclaration, (n) => !n.decorators || n.decorators.findIndex(x => x.expression.type === 'CallExpression' && x.expression.callee.type === 'Identifier' && x.expression.callee.name === map1.get('Entity')) === -1).forEach((n: namedTypes.ClassDeclaration) => {
+    console.log(n.type);
+        if(n.decorators) {
+            n.decorators.push(j.decorator(j.callExpression(j.identifier(map1.get('Entity')), [])));
+        } else {
+            n.decorators = [j.decorator(j.callExpression(j.identifier(map1.get('Entity')), []))];
+        }
+    });*/
+/*    
+    c.find(j.ClassProperty).nodes().forEach((n: namedTypes.ClassProperty) => {
+        if(n.decorators) {
+            n.decorators.push(j.decorator(j.callExpression(j.identifier(map1.get('Column')), [])));
+        } else {
+            n.decorators = [j.decorator(j.callExpression(j.identifier(map1.get('Column')), []))];
+        }
+    });
+  */
+  return c.toSource();
+//    return print(ast).code;
 }
