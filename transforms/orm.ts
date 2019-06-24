fcd6a5b26df329c77bbe0ea49ed014fe477e13a1
Kay McCormick <kay@kaymccormick.com>
2019-06-24 03:18:03 +0000

    support various kinds of parsing

--- transforms/orm.ts
+++ transforms/orm.ts
@@ -9,17 +9,21 @@ import { fromNodes } from 'jscodeshift/src/Collection';
 // @ts-ignore
 module.exports = function (fileInfo, api, options) {
     const report = api.report;
-//    const ast = parse(fileInfo.source);
+    let ast;
     const j = api.jscodeshift;
-    
-    const c = j(fileInfo.source);
+    let c;
+    if(options['parse-with'] === 'recast') {
+       ast = parse(fileInfo.source);
+       c = j(ast);
+    } else {
+      c = j(fileInfo.source);
+      }
+
     const m = c.find(j.ImportDeclaration, (n: namedTypes.ImportDeclaration) => {
-    console.log(n);
-        return n.source.type === 'StringLiteral'
+        return n.source.type === (ast ? 'Literal' : 'StringLiteral')
 		&& n.source && n.source.value === 'typeorm'
  		&& n.specifiers });
 		let map2: Map<string, string> = Map<string, string>();
-    console.log(m.size());
     let map1: Map<string, string> = Map<string, string>(
     	  List.of<SpecifierKind>(...m.nodes()
             .flatMap((n: namedTypes.ImportDeclaration): SpecifierKind[] =>
@@ -28,8 +32,7 @@ module.exports = function (fileInfo, api, options) {
                 specifier.type === 'ImportSpecifier' &&
                 specifier.imported && specifier.local ? [specifier.imported.name, specifier.local.name] : [])
             .filter(x => x.length));
-    console.log('map1: ', map1.toJS());
-    
+
     const imports = ['Column', 'Entity'];
     imports.forEach(importName => {
     if(!map1.has(importName) || map1.get(importName) === undefined) {
@@ -37,29 +40,34 @@ module.exports = function (fileInfo, api, options) {
     }
     });
     if(map2.count()) {
-    console.log(map2.toJS());
     c.nodes()[0].program.body.splice(0, 0, j.importDeclaration(map2.map((importedName, importName) => j.importSpecifier(j.identifier(importedName), j.identifier(importName))).valueSeq().toJS(), j.literal('typeorm')));
     }
     map1 = map1.merge(map2);
-    console.log(map1.toJS())
 
-/*    c.find(j.ClassDeclaration, (n) => !n.decorators || n.decorators.findIndex(x => x.expression.type === 'CallExpression' && x.expression.callee.type === 'Identifier' && x.expression.callee.name === map1.get('Entity')) === -1).forEach((n: namedTypes.ClassDeclaration) => {
+    c.find(j.ClassDeclaration, (n) => {
+    return !n.decorators || n.decorators.findIndex(x => x.expression.type === 'CallExpression' && x.expression.callee.type === 'Identifier' && x.expression.callee.name === map1.get('Entity')) === -1;
+    }).forEach((n: namedTypes.ClassDeclaration) => {
     console.log(n.type);
         if(n.decorators) {
             n.decorators.push(j.decorator(j.callExpression(j.identifier(map1.get('Entity')), [])));
         } else {
             n.decorators = [j.decorator(j.callExpression(j.identifier(map1.get('Entity')), []))];
         }
-    });*/
-/*    
+    });
+
     c.find(j.ClassProperty).nodes().forEach((n: namedTypes.ClassProperty) => {
+    console.log(n.type);
         if(n.decorators) {
             n.decorators.push(j.decorator(j.callExpression(j.identifier(map1.get('Column')), [])));
         } else {
             n.decorators = [j.decorator(j.callExpression(j.identifier(map1.get('Column')), []))];
         }
     });
-  */
+
+  if(ast) {
+    return print(ast).code;
+  } else {
   return c.toSource();
+  }
 //    return print(ast).code;
 }
