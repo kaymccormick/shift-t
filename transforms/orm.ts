307754063f777214da5fb738749c3b2ab627961c
Kay McCormick <kay@kaymccormick.com>
2019-06-24 01:41:19 +0000

    update

--- transforms/orm.ts
+++ transforms/orm.ts
@@ -4,16 +4,20 @@ import { builders} from 'ast-types';
 import {Collection, Map, List} from 'immutable';
 import {NodePath} from "ast-types/lib/node-path";
 import {SpecifierKind} from "ast-types/gen/kinds";
+import { fromNodes } from 'jscodeshift/src/Collection';
 
 // @ts-ignore
 module.exports = function (fileInfo, api, options) {
     const report = api.report;
+    const ast = parse(fileInfo.source);
     const j = api.jscodeshift;
     
-    const c = j(fileInfo.source);
-
-    const m = c.find(j.ImportDeclaration, (n: namedTypes.ImportDeclaration) => {
+    const c = j(ast);
+    console.log(c.find(j.ImportDeclaration, (n: namedTypes.ImportDeclaration) => {
                 return n.source.type === 'StringLiteral'
+ 		}).size());
+    const m = c.find(j.ImportDeclaration, (n: namedTypes.ImportDeclaration) => {
+                return n.source.type === 'Literal'
 		&& n.source && n.source.value === 'typeorm'
  		&& n.specifiers });
     console.log(m.size());
@@ -33,7 +37,7 @@ module.exports = function (fileInfo, api, options) {
     const fileNode = c.nodes()[0];
     if(!map1.has('Entity')) {
       console.log('here');
-        fileNode.program.body.splice(0, j.importDeclaration([j.importSpecifier(j.identifier('Entity'))], j.literal('typeorm')));
+        ast.program.body.splice(0, 0, j.importDeclaration([j.importSpecifier(j.identifier('Entity'))], j.literal('typeorm')));
 	}
     
     c.find(j.ImportDeclaration, (n) => n.importKind === 'value' && n.source.type === 'StringLiteral' && n.source.value === 'typeorm').forEach((p: NodePath) => {
@@ -41,7 +45,7 @@ module.exports = function (fileInfo, api, options) {
     });
     //    c.find(j.ClassDeclaration).forEach((p: NodePath) => {
     //      const classDecl = p.value;
-    return print(c.nodes()[0]).code;
+    return print(ast).code;
 //    return c.toSource();
 //    return api.jscodeshift(
 //    return print(c.nodes()[0]).code;
