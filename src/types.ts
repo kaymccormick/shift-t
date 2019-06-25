c26a826a53847e578636e7768d8b6b7076369182
Kay McCormick <kay@kaymccormick.com>
2019-06-24 17:22:40 -0700

    types

---
+++ src/types.ts
@@ -0,0 +1,9 @@
+export type ModuleSpecifier = string;
+
+export interface ImportContext {
+  module: ModuleSpecifier;
+}
+
+export interface HandleImportSpecifier {
+  (importContext: ImportContext, localName: string, importName: string, isDefault?: boolean, isNamespace?: boolean) => void;
+}
