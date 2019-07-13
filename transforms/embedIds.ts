c77173621b489c2ffbe4326720ccf68677e8f4e0
Kay McCormick <kay@kaymccormick.com>
2019-07-13 22:07:03 +0000

    update

--- transforms/embedIds.ts
+++ transforms/embedIds.ts
@@ -5,40 +5,61 @@ import assert from 'assert';
 import EntityCore from "classModel/lib/src/entityCore";
 import  winston, { format } from 'winston';
 import finder from 'find-package-json';
-import { builders as b } from 'ast-types';
+import {builders as b, NodePath} from 'ast-types';
 import { copyTree }from '../src/utils';
 import {StatementKind} from "ast-types/gen/kinds";
-import {namedTypes} from 'ast-types';
+import {namedTypes,visit} from 'ast-types';
 
 
-const consoleTransport  = new winston.transports.Console({level: 'warn'});
+const consoleTransport  = new winston.transports.Console({level: 'error'});
 const file = new winston.transports.File({level: 'debug', filename:
       'embed.log'})
 const loggerTranports = [consoleTransport, file];
 const logger = winston.createLogger({format: format.json(),
     transports:loggerTranports });
 
-function embedUuidInComment(j, uuid: string, node: namedTypes.Node) {
-         const comments = node.comments;
+function embedUuidInComment(j, uuid: string, nodePath: NodePath<namedTypes.Node>, node: namedTypes.Node, args: any) {
+    const { lastComment }  = args;
+    if(uuid === undefined) {
+        throw new Error('invalid uuid');
+    }
+    if(nodePath) {
+        if(nodePath.value !== nodePath.node ) {
+            throw new Error('eep');
+        }
+        const p =  nodePath.parentPath;
+        console.log(`XX ${nodePath}`);
+    }
+    const comments = node.comments;
     if(comments && comments.length) {
-                const comment = comments[0];
-                const val = comment.value;
-                const match = /@uuid\s+(\S+)/.exec(val);
-                if(match) {
-                    if(match[1] !== module.uuid) {
-                      node.comments[0].value = val.replace(/@uuid\s+(\S+)/, `@uuid ${module.uuid}`);
-                      }
-                    
-                } else {
-                    const newVal = `${val}\n * @uuid ${module.uuid}\n`;
-                        node.comments[0].value = newVal;
-                        console.log(newVal);
-                }
-            } else {
-            const comment = j.commentBlock(`* @uuid ${module.uuid}\n`);
+        let commentIndex = lastComment? comments.length - 1 : 0;
+        if(lastComment && commentIndex === 0) {
+            node.comments.splice(0, 0, j.commentBlock('empty'));
+            commentIndex += 1;
+        }
+
+        const comment = comments[commentIndex];
+        const val = comment.value;
+        const match = /@uuid\s+(\S+)/.exec(val);
+        if(match) {
+            if(match[1] !== uuid) {
+                node.comments[commentIndex].value = val.replace(/@uuid\s+(\S+)/, `@uuid ${uuid}`);
+            }
+
+        } else {
+            const newVal = `${val}\n * @uuid ${uuid}\n`;
+            node.comments[commentIndex].value = newVal;
+            console.log(newVal);
+        }
+    } else {
+        const comment = j.commentBlock(`* @uuid ${uuid}\n`);
+        if(lastComment) {
+            node.comments = [j.commentBlock('empty'), comment];
+        } else {
             node.comments = [comment];
-           }
-           }
+        }
+    }
+}
 
 module.exports = function(fileInfo, api, options) {
     const report = api.report;
@@ -59,20 +80,24 @@ module.exports = function(fileInfo, api, options) {
     }
 
     const p =  path.resolve(fileInfo.path);
-    const r2 = path.relative(path.dirname(pkgFile), p);
-    const name = p.replace(/\.tsx?$/, '');
-
     const model = JSON.parse(fs.readFileSync('dump.json', { encoding: 'utf-8'}));
     const project = model.Project.find((project): boolean => {
-    console.log(project);
-    return project.name === packageName;
+        console.log(project);
+        return project.name === packageName;
     });
     if(!project) {
-    throw new Error('no project');
+        throw new Error('no project');
     }
+    if(!project.path) {
+        throw new Error('no project path');
+    }
+
+    const r2 = path.relative(project.path!, p);
+    const name = r2.replace(/\.tsx?$/, '');
+
     const module = model.Module.find((module): boolean => module.name === name);
     if(!module) {
-    throw new Error(`no module ${name}`);
+        throw new Error(`no module ${name}`);
     }
 
     const body = r.nodes()[0].program.body;
@@ -81,27 +106,39 @@ module.exports = function(fileInfo, api, options) {
         const newStmt = copyTree(stmt, logger).toJS();
         console.log(JSON.stringify(newStmt));
         if(index === 0) {
-            embedUuidInComment(j, module.uuid, newStmt);
+            embedUuidInComment(j, module.uuid, undefined, newStmt, {});
         }
         newBody.push(newStmt);
     });
 
-     j(newBody).find(namedTypes.ClassDeclaration).nodes().forEach((classDecl): void => {
-     const class_ = model['Class'].find((class__) => {
-     return class__.moduleId === module.id && classDecl.id && class__.name === classDecl.id.name;
-     });
-     if(!class_) {
-     console.log(classDecl.id!.name);
-console.log(model.Class.filter((class__) => class__.moduleId == module.id));
-     throw new Error('no class');
-     }
-     
-     });
-     
 
 
 
     const newFile = b.file(b.program(newBody));
+
+    visit( newFile, {
+        visitClassDeclaration(path: NodePath<namedTypes.ClassDeclaration>): boolean {
+            const classDecl = path.node;
+            let theNode = classDecl;
+            let thePath = path;
+            const parent = path.parent;
+            if(parent && parent.node && (parent.node.type === 'ExportDefaultDeclaration' || parent.node.type === 'ExportNamedDeclaration')) {
+                theNode = parent.node;
+                thePath = parent;
+            }
+            const class_ = model['Class'].find((class__) => {
+                return class__.moduleId === module.id && classDecl.id && class__.name === classDecl.id.name;
+            });
+            if(!class_) {
+                console.log(classDecl.id!.name);
+                console.log(model.Class.filter((class__) => class__.moduleId == module.id));
+                throw new Error('no class');
+            }
+
+            embedUuidInComment(j, class_.uuid, thePath, theNode, { lastComment: true });
+            return false;
+        },
+    });
     try {
         const xx = print(newFile);
         const code = xx.code;
