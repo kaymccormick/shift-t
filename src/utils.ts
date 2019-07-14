f78a23e14a270f4a212bc52c998ad1602ea45ef1
Kay McCormick <kay@kaymccormick.com>
2019-07-14 01:25:17 +0000

    do copy

--- src/utils.ts
+++ src/utils.ts
@@ -1,5 +1,6 @@
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
-import { getFieldNames, getFieldValue, eachField, namedTypes } from 'ast-types';
+import {getFieldNames, getFieldValue, eachField, namedTypes, NodePath, builders} from 'ast-types';
+import { CommentKind } from 'ast-types/gen/kinds';
 import { Map,List } from 'immutable';
 import { ValueKind, BasicLogger } from './types';
 
@@ -27,17 +28,63 @@ export function copyTree(node: namedTypes.Node, logger?: BasicLogger, level: num
         && value.constructor && value.constructor.name === "Node") {
             out = out.set(name, copyTree(value, logger, level + 1));
         } else if(value && value.type) {
-            throw new Error(`busted copyTree with ${value.type}`);
+            out = out.set(name, copyTree(value, logger, level + 1));
         } else {{
             out = out.set(name, value);
         }
         }
     });
     if(logger) {
-    //    logger.debug(`copyTree returning ${JSON.stringify(out, null, 4)}`);
+        logger.debug(`copyTree returning ${JSON.stringify(out, null, 4)}`);
     }
     return out;
 }
 
-          
-     
+export function normalizeDocComment(report, lines) {
+    let newLines = lines.map(line => line.replace(/^\s*\*\s*/, ''));
+    if(newLines[0].trim()) {
+        newLines.splice(0, 0, '');
+    }
+    if(newLines[newLines.length - 1].trim()) {
+        newLines.splice(newLines.length, 0, ' ');
+    } else {
+        newLines[newLines.length - 1] = ' ';
+    }
+
+    newLines = newLines.map((line, index) => index === 0 ? `*` : index === newLines.length -1 ? line : ` * ${line}`);
+    return newLines;
+}
+
+export function processComments(report, nodePath: NodePath<namedTypes.Node>) {
+    const node = nodePath.node;
+    const comments = node.comments;
+    if(!comments || !comments.length) {
+        return;
+    }
+    comments.forEach((comment: CommentKind): void => {
+        if(comment.type === "CommentBlock" || comment.type == "Block") {
+            const val = comment.value;
+            report(val.replace(/\n/g, '\\n'));
+            let containsAtUuid = val.indexOf('@uuid') !== -1;
+            const lines = val.split(/\r?\n/g);
+            let docComment = false;
+            let singleLine = false;
+            if(lines[0] === '*') {
+                docComment = true;
+            }
+            if(lines.length === 1) {
+                singleLine = true;
+            }
+            //report(`${singleLine} ${docComment} ${containsAtUuid}`);
+            if(containsAtUuid || docComment) {
+                if(!containsAtUuid) {
+                    report('here');
+                }
+                const newLines = normalizeDocComment(report, lines);
+                const newVal = newLines.join('\n');
+                //  report(`processComments: ${newVal}\n.\n`);
+                comment.value = newVal;
+            }
+        }
+    });
+}
